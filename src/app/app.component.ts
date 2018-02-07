d2192ff3b3d1af6aac4c2462369a11a58eb904d2
tahir94 <tahirkhan.on@gmail.com>
2018-02-07 19:27:16 +0500

    Your Awesome Changes!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class MyApp {
 //   isHomeNav  : boolean;
   @ViewChild(Nav) nav: Nav;
 
-  rootPage : any = HomePage;
+  rootPage : any = LoginPage;
   pages : Array<{title: string, component: any}>
 
 
